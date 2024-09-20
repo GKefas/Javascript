8df481050369ba55e567f9c3008efc0b0250a11c
George Kefas <kefasgeo04@gmail.com>
2024-09-20 16:25:25 +0300

    adding form on click

--- Mapty/script.js
+++ Mapty/script.js
@@ -11,6 +11,8 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
@@ -18,28 +20,50 @@ if (navigator.geolocation)
       const coords = [latitude, longitude];
       const mapTileUrl = 'https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png';
 
-      const map = L.map('map').setView(coords, 10);
+      map = L.map('map').setView(coords, 10);
 
       L.tileLayer(mapTileUrl).addTo(map);
 
-      map.on('click', function (mapEvent) {
-        const { lat, lng } = mapEvent.latlng;
-        const popupOptions = {
-          maxWidth: 250,
-          minWidth: 100,
-          autoClose: false,
-          closeOnClick: false,
-          className: 'running-popup',
-        };
-
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(L.popup(popupOptions))
-          .setPopupContent('Workout')
-          .openPopup();
+      // Handling clicks on map
+      map.on('click', function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
       });
     },
     function () {
       alert('Could not get your position');
     }
   );
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  // Clear input fields
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+
+  // Display marker
+  const { lat, lng } = mapEvent.latlng;
+  const popupOptions = {
+    maxWidth: 250,
+    minWidth: 100,
+    autoClose: false,
+    closeOnClick: false,
+    className: 'running-popup',
+  };
+
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(L.popup(popupOptions))
+    .setPopupContent('Workout')
+    .openPopup();
+});
+
+inputType.addEventListener('change', function () {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
