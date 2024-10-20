c9b796d232d8adbb753c3ec52f29aa70657b4870
George Kefas <kefasgeo04@gmail.com>
2024-10-20 20:47:20 +0300

    setting up pagination

--- Forkify/src/js/index.js
+++ Forkify/src/js/index.js
@@ -41,7 +41,7 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3) Render Results
-    resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage(1));
   } catch (err) {
     resultsView.renderError(
       `Searching recipes took too long! Timeout after ${TIMEOUT_SEC} seconds`
--- Forkify/src/js/model.js
+++ Forkify/src/js/model.js
@@ -1,10 +1,12 @@
-import { API_URL } from './config';
+import { API_URL, RES_PER_PAGE } from './config';
 import { getJSON } from './helpers';
 const state = {
   recipe: {},
   search: {
     query: '',
     results: [],
+    page: 1,
+    resultsPerPage: RES_PER_PAGE,
   },
 };
 
@@ -69,4 +71,11 @@ const loadSearchResults = async function (query) {
   }
 };
 
-export { state, loadRecipe, loadSearchResults };
+const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+  const start = (page - 1) * state.search.resultsPerPage;
+  const end = state.search.resultsPerPage;
+  return state.search.results.slice(start, end);
+};
+
+export { state, loadRecipe, loadSearchResults, getSearchResultsPage };
